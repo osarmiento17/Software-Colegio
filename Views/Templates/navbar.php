af0e582cac3c1bca5a2531d52bb76be5401e94b2
Jaime Correa <correaorozcojaime@gmail.com>
2020-05-07 19:31:01 -0500

    Se diseña la interfaz del Index de la aplicación en donde vemos el total de Cursos, Profesores y alumnos registrados. Se añade la barra de navegación para movernos entre módulos del software.

---
+++ Views/Templates/navbar.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <span href="#" class="navbar-brand mb-0 h1">Software Colegio</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSoftware">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSoftware">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a href="#" class="nav-link"><i class="fas fa-user-edit"></i> Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link"><i class="fas fa-calendar-check"></i> Asistencia</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown"><i class="fas fa-sliders-h"></i> Gestionar</a>
+                    <div class="dropdown-menu">
+                        <a href="#" class="dropdown-item"><i class="fas fa-chalkboard"></i> Cursos</a>
+                        <a href="#" class="dropdown-item"><i class="fas fa-chalkboard-teacher"></i> Profesores</a>
+                        <a href="#" class="dropdown-item"><i class="fas fa-user-graduate"></i> Estudiantes</a>
+                    </div>
+                </li>
+            </ul>        
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">Hola, Usuario</a>
+                    <div class="dropdown-menu">
+                        <a href="#" class="dropdown-item disabled"><i class="fas fa-user-edit"></i> Editar perfil</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item"><i class="fas fa-sign-out-alt"></i> Cerrar sesi&oacute;n</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<br>
+<br>
--- Views/index.php
+++ Views/index.php
@@ -1,3 +1,50 @@
+<?php 
+    date_default_timezone_set('America/Bogota');
+
+    $dia = date('d');
+    $mes = date('m');
+    $ano = date('Y');
+    
+    switch($mes){
+        case '01':
+            $mes = 'Enero';
+        break;
+        case '02':
+            $mes = 'Febrero';
+        break;
+        case '03':
+            $mes = 'Marzo';
+        break;
+        case '04':
+            $mes = 'Abril';
+        break;
+        case '05':
+            $mes = 'Mayo';
+        break;
+        case '06':
+            $mes = 'Junio';
+        break;
+        case '07':
+            $mes = 'Julio';
+        break;
+        case '08':
+            $mes = 'Agosto';
+        break;
+        case '09':
+            $mes = 'Septiembre';
+        break;
+        case '10':
+            $mes = 'Octubre';
+        break;
+        case '11':
+            $mes = 'Noviembre';
+        break;
+        case '12':
+            $mes = 'Diciembre';
+        break;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,36 +52,84 @@
     <title>Inicio | Software Colegio</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
-        <span href="#" class="navbar-brand mb-0 h1">Software Colegio</span>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSoftware">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSoftware">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a href="#" class="nav-link">Inicio</a>
-                </li>
-                <li class="nav-item active">
-                    <a href="#" class="nav-link">Asistencia</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">Gestionar</a>
-                    <div class="dropdown-menu">
-                        <a href="#" class="dropdown-item">Cursos</a>
-                        <a href="#" class="dropdown-item">Profesores</a>
-                        <a href="#" class="dropdown-item">Estudiantes</a>
-                    </div>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Cerrar sesi&oacute;n</a>
-                </li>
-            </ul>
+    <?php include_once './Templates/navbar.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Panel de Administraci&oacute;n</h2>
+            </div>
         </div>
-    </nav>
-
+        <br>
+        <br>
+        <div class="row mb-5">
+            <div class="col-sm-12">
+                <button class="btn btn-primary btn-lg btn-block">
+                    <h4 class="card-title text-center">
+                        <i class="fas fa-calendar-alt"></i> Asistencia de hoy <?= "$dia de $mes de $ano."; ?>
+                    </h4>
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-4 mt-1 mb-1">
+                <div class="card shadow bg-info text-white">
+                    <div class="row no-glutters">
+                        <div class="col-6">
+                            <i class="fas fa-chalkboard fa-4x mt-4 ml-5"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="card-body">
+                                <p class="card-text display-4">0</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="card-link text-white">
+                        <div class="card-footer text-center">
+                            <h4>Cursos</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 mt-1 mb-1">
+                <div class="card shadow bg-success text-white">
+                    <div class="row no-glutters">
+                        <div class="col-6">
+                            <i class="fas fa-chalkboard-teacher fa-4x mt-4 ml-5"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="card-body">
+                                <p class="card-text display-4">0</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="card-link text-white">
+                        <div class="card-footer text-center">
+                            <h4>Profesores</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 mt-1 mb-1">
+                <div class="card shadow bg-secondary text-white">
+                    <div class="row no-glutters">
+                        <div class="col-6">
+                            <i class="fas fa-user-graduate fa-4x mt-4 ml-5"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="card-body">
+                                <p class="card-text display-4">0</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="card-link text-white">
+                        <div class="card-footer text-center">
+                            <h4>Alumnos</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include_once './Templates/scripts.php'; ?>
 </body>
 </html>
